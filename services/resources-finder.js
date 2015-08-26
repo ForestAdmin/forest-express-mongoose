83e2d79407d493e6e72c1233358ea4bc21aeab9d
Sandro Munda <sandro@munda.me>
2015-08-26 10:09:04 +0200

    Users search values

--- services/resources-finder.js
+++ services/resources-finder.js
@@ -12,42 +12,98 @@ function ResourcesFinder(model, opts, params) {
     });
   }
 
-  function count () {
+  function count (query) {
     return new P(function (resolve, reject) {
-      model.count(function (err, count) {
+      query.count(function (err, count) {
         if (err) { return reject(err); }
         resolve(count);
       });
     });
   }
 
-  function getRecords() {
-    return new P(function (resolve, reject) {
-      var query = model.find();
-
-      query
-        .limit(getLimit())
-        .skip(getSkip());
+  function handleSearchParam(query) {
+    if (new RegExp('^[0-9a-fA-F]{24}$').test(params.search)) {
+      query.where({ _id: params.search });
+    } else {
+      var orQuery = { $or: [] };
 
       _.each(model.schema.paths, function (value, key) {
-        if (MongooseUtils.getReference(value)) {
-          query = query.populate(key);
+        if (value.instance === 'String') {
+          var q = {};
+          q[key] = new RegExp('.*' + params.search + '.*', 'i');
+
+          orQuery.$or.push(q);
         }
       });
 
+      query.where(orQuery);
+    }
+  }
 
-      if (params.sort) {
-        if (params.sort.split('.').length > 1) {
-          query.sort(params.sort.split('.')[0]);
-        } else {
-          query.sort(params.sort);
-        }
+  function handleFilterParams(query) {
+    _.each(params.filter, function (value, key) {
+      var q = {};
+
+      if (value[0] === '!') {
+        q[key] = { $ne: value.substring(1) };
+      } else if (value[0] === '*' && value[value.length - 1] === '*') {
+        q[key] = new RegExp('.*' + value.substring(1, value.length - 1) +
+          '.*');
+      } else if (value[0] === '*') {
+        q[key] = new RegExp('.*' + value.substring(1) + '$');
+      } else if (value[value.length - 1]) {
+        q[key] = new RegExp('^' + value.substring(0, value.length - 1) +
+          '.*');
+      } else {
+        q[key] = value;
       }
 
-      query.lean().exec(function (err, records) {
-        if (err) { return reject(err); }
-        resolve(records);
-      });
+      query.where(q);
+    });
+  }
+
+  function handleSortParam(query) {
+    if (params.sort.split('.').length > 1) {
+      query.sort(params.sort.split('.')[0]);
+    } else {
+      query.sort(params.sort);
+    }
+  }
+
+  function getRecords() {
+    var query = model.find();
+
+    _.each(model.schema.paths, function (value, key) {
+      if (MongooseUtils.getReference(value)) {
+        query = query.populate(key);
+      }
+    });
+
+    if (params.search) {
+      handleSearchParam(query);
+    }
+
+    if (params.filter) {
+      handleFilterParams();
+    }
+
+    if (params.sort) {
+      handleSortParam();
+    }
+
+    return query;
+  }
+
+  function exec(query) {
+    return new P(function (resolve, reject) {
+      query
+        .limit(getLimit())
+        .skip(getSkip())
+        .lean()
+        .exec(function (err, records) {
+          if (err) { return reject(err); }
+          resolve(records);
+        });
     });
   }
 
@@ -76,7 +132,8 @@ function ResourcesFinder(model, opts, params) {
     if (hasManyParam) {
       return new HasManyFinder(model, hasManyParam, opts, params).perform();
     } else {
-      return new P.all([count(), getRecords()]);
+      var query = getRecords();
+      return new P.all([count(query), exec(getRecords())]);
     }
   };
 }
