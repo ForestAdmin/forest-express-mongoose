345f0a58391de0971c6fb8942ddf4796f8e489b4
Sandro Munda <sandro@munda.me>
2015-11-03 14:39:48 +0100

    Push the apimap on start

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ var bodyParser = require('body-parser');
 var jwt = require('express-jwt');
 var ResourcesRoutes = require('./routes/resources');
 var AssociationsRoutes = require('./routes/associations');
-var ApimapRoutes = require('./routes/apimap');
 var StripeRoutes = require('./routes/stripe');
 var StatRoutes = require('./routes/stats');
 var Schemas = require('./generators/schemas');
+var SchemaAdapter = require('./adapters/mongoose');
+var JSONAPISerializer = require('jsonapi-serializer');
+var request = require('superagent');
+var logger = require('./services/logger');
 
 function requireAllModels(modelsDir, opts) {
   return fs.readdirAsync(modelsDir)
@@ -24,6 +27,15 @@ function requireAllModels(modelsDir, opts) {
     .then(function () {
       return _.values(opts.mongoose.models);
     });
+}
+
+function mapSeries(things, fn) {
+  var results = [];
+  return P.each(things, function (value, index, length) {
+    var ret = fn(value, index, length);
+    results.push(ret);
+    return ret;
+  }).thenReturn(results).all();
 }
 
 exports.init = function (opts) {
@@ -58,7 +70,36 @@ exports.init = function (opts) {
       return models;
     })
     .then(function (models) {
-      return new ApimapRoutes(app, models, opts).perform();
+      if (opts.jwtSigningKey) {
+        mapSeries(models, function (model) {
+          return new SchemaAdapter(model, opts);
+        })
+        .then(function (collections) {
+          return new JSONAPISerializer('collections', collections, {
+            id: 'name',
+            attributes: ['name', 'fields'],
+            fields: {
+              attributes: ['field', 'type', 'collection_name']
+            }
+          });
+        })
+        .then(function (json) {
+          var forestUrl = process.env.FOREST_URL ||
+            'https://www.forestadmin.com';
+
+          request
+            .post(forestUrl + '/forest/apimaps')
+              .send(json)
+              .set('forest-secret-key', opts.jwtSigningKey)
+              .end(function(err, res) {
+                if (res.status !== 204) {
+                  logger.debug('Forest cannot find your project secret key. ' +
+                    'Please, ensure you have installed the Forest Liana ' +
+                    'correctly.');
+                }
+              });
+        });
+      }
     });
 
 
