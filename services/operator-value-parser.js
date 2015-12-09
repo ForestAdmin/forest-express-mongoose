7e4ffa25dfa882f11ede441a30248bdfb3931eb6
Sandro Munda <sandro@munda.me>
2015-12-09 12:16:25 +0100

    Support relative datetime

--- services/operator-value-parser.js
+++ services/operator-value-parser.js
@@ -1,6 +1,7 @@
 'use strict';
 var _ = require('lodash');
 var Schemas = require('../generators/schemas');
+var moment = require('moment');
 
 function OperatorValueParser(opts) {
 
@@ -34,20 +35,43 @@ function OperatorValueParser(opts) {
       }
     }
 
+    function getValue() {
+      switch (value) {
+        case 'yesterday':
+          return moment().subtract(1, 'days').toDate();
+        case 'lastWeek':
+          return moment().subtract(1, 'weeks').toDate();
+        case 'last2Weeks':
+          return moment().subtract(2, 'weeks').toDate();
+        case 'lastMonth':
+          return moment().subtract(1, 'months').toDate();
+        case 'last3Months':
+          return moment().subtract(3, 'months').toDate();
+        case 'lastYear':
+          return moment().subtract(1, 'years').toDate();
+        default:
+          return value;
+      }
+    }
+
     if (value[0] === '!') {
-      ret = { $ne: parseFct(value.substring(1)) };
+      value = value.substring(1);
+      ret = { $ne: parseFct(getValue()) };
     } else if (value[0] === '>') {
-      ret = { $gt: parseFct(value.substring(1)) };
+      value = value.substring(1);
+      ret = { $gt: parseFct(getValue()) };
     } else if (value[0] === '<') {
-      ret = { $lt: parseFct(value.substring(1)) };
+      value = value.substring(1);
+      ret = { $lt: parseFct(getValue()) };
     } else if (value[0] === '*' && value[value.length - 1] === '*') {
-      ret = new RegExp('.*' + parseFct(value.substring(1, value.length - 1)) +
-        '.*');
+      value = value.substring(1, value.length - 1);
+      ret = new RegExp('.*' + parseFct(getValue()) + '.*');
     } else if (value[0] === '*') {
-      ret = new RegExp('.*' + parseFct(value.substring(1)) + '$');
+      value = value.substring(1);
+      ret = new RegExp('.*' + parseFct(getValue()) + '$');
     } else if (value[value.length - 1] === '*') {
-      ret = new RegExp('^' + parseFct(value.substring(0, value.length - 1)) +
-        '.*');
+      value = value.substring(0, value.length - 1);
+      ret = new RegExp('^' + parseFct(getValue()) + '.*');
     } else if (value === '$present') {
       ret = { $exists: true };
     } else if (value === '$blank') {
