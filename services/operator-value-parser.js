a136cb3ec408308ab2de082ec06cc6d7fe73d1c0
Sandro Munda <sandro@munda.me>
2015-12-16 11:02:59 +0100

    Fix the lastWeek/lastMonth/...

--- services/operator-value-parser.js
+++ services/operator-value-parser.js
@@ -39,43 +39,73 @@ function OperatorValueParser(opts) {
       }
     }
 
-    function getValue() {
+    function getIntervalDateValue(value) {
+      var from = null;
+      var to = null;
+
       switch (value) {
         case 'yesterday':
-          return moment().subtract(1, 'days').toDate();
+          from = moment().subtract(1, 'days').startOf('day').toDate();
+          to = moment().subtract(1, 'days').endOf('day').toDate();
+          break;
         case 'lastWeek':
-          return moment().subtract(1, 'weeks').toDate();
+          from = moment().subtract(1, 'weeks').startOf('isoWeek').toDate();
+          to = moment().subtract(1, 'weeks').endOf('isoWeek').toDate();
+          break;
         case 'last2Weeks':
-          return moment().subtract(2, 'weeks').toDate();
+          from = moment().subtract(2, 'weeks').startOf('isoWeek').toDate();
+          to = moment().subtract(1, 'weeks').endOf('isoWeek').toDate();
+          break;
         case 'lastMonth':
-          return moment().subtract(1, 'months').toDate();
+          from = moment().subtract(1, 'months').startOf('month').toDate();
+          to = moment().subtract(1, 'months').endOf('month').toDate();
+          break;
         case 'last3Months':
-          return moment().subtract(3, 'months').toDate();
+          from = moment().subtract(3, 'months').startOf('month').toDate();
+          to = moment().subtract(1, 'months').endOf('month').toDate();
+          break;
         case 'lastYear':
-          return moment().subtract(1, 'years').toDate();
-        default:
-          return value;
+          from = moment().subtract(1, 'years').startOf('year').toDate();
+          to = moment().subtract(1, 'years').endOf('year').toDate();
+          break;
       }
+
+      return { $gte: from, $lte: to };
+    }
+
+    function isIntervalDateValue(value) {
+      return ['yesterday', 'lastWeek', 'last2Weeks', 'lastMonth',
+        'last3Months', 'lastYear'].indexOf(value) > -1;
     }
 
     if (value[0] === '!') {
       value = value.substring(1);
-      ret = { $ne: parseFct(getValue()) };
+      ret = { $ne: parseFct(value) };
     } else if (value[0] === '>') {
       value = value.substring(1);
-      ret = { $gt: parseFct(getValue()) };
+
+      if (isIntervalDateValue(value)) {
+        ret = getIntervalDateValue(value);
+      } else {
+        ret = { $gt: parseFct(value) };
+      }
     } else if (value[0] === '<') {
       value = value.substring(1);
-      ret = { $lt: parseFct(getValue()) };
+
+      if (isIntervalDateValue(value)) {
+        ret = getIntervalDateValue(value);
+      } else {
+        ret = { $lt: parseFct(value) };
+      }
     } else if (value[0] === '*' && value[value.length - 1] === '*') {
       value = value.substring(1, value.length - 1);
-      ret = new RegExp('.*' + parseFct(getValue()) + '.*');
+      ret = new RegExp('.*' + parseFct(value) + '.*');
     } else if (value[0] === '*') {
       value = value.substring(1);
-      ret = new RegExp('.*' + parseFct(getValue()) + '$');
+      ret = new RegExp('.*' + parseFct(value) + '$');
     } else if (value[value.length - 1] === '*') {
       value = value.substring(0, value.length - 1);
-      ret = new RegExp('^' + parseFct(getValue()) + '.*');
+      ret = new RegExp('^' + parseFct(value) + '.*');
     } else if (value === '$present') {
       ret = { $exists: true };
     } else if (value === '$blank') {
