07a61d73529f1f0639d8d3be8a6318639e94c81b
Sandro Munda <sandro@munda.me>
2016-03-09 11:17:02 +0100

    Fix the activity logger

--- services/activity-log-logger.js
+++ services/activity-log-logger.js
@@ -4,15 +4,12 @@ var request = require('superagent');
 var jwt = require('jsonwebtoken');
 
 function ActivityLogLogger(opts) {
-  function getProjectId(user) {
-    return user.session.data.relationships.project.data.id;
-  }
-
   this.perform = function (user, action, collectionName, resource) {
     var json = {
       action: action,
       collection: collectionName,
-      resource: resource
+      resource: resource,
+      user: user.id
     };
 
     var token = jwt.sign(user, opts.jwtSigningKey);
@@ -20,12 +17,15 @@ function ActivityLogLogger(opts) {
     var forestUrl = process.env.FOREST_URL ||
       'https://forestadmin-server.herokuapp.com';
 
+    console.log(require('util').inspect(opts.secretKey, { depth: null }));
+
     return new P(function (resolve, reject) {
       request
-        .post(forestUrl + '/api/projects/' + getProjectId(user) +
-          '/activity-logs')
+        .post(forestUrl + '/api/activity-logs')
         .send(json)
         .set('Authorization', 'Bearer ' + token)
+        //.set('Content-Type', 'application/vnd.api+json')
+        .set('forest-secret-key', opts.secretKey)
         .end(function(err, res) {
           if (err) { return reject(err); }
           resolve(res);
