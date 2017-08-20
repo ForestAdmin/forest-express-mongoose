6929d86dda5c05221e1497b7eb217c1295734436
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-08-20 22:11:54 +0200

    Support hasMany Exports

--- services/records-exporter.js
+++ services/records-exporter.js
@@ -1,16 +1,25 @@
 'use strict';
 var ResourcesGetter = require('./resources-getter');
+var HasManyGetter = require('./has-many-getter');
 
 var BATCH_INITIAL_PAGE = 1;
 var BATCH_SIZE = 1000;
 
-function RecordsExporter(model, options, params) {
+function RecordsExporter(model, options, params, association) {
   params.sort = '_id';
   params.page = { size: BATCH_SIZE };
 
+  function getter() {
+    if (association) {
+      return new HasManyGetter(model, association, options, params);
+    } else {
+      return new ResourcesGetter(model, options, params);
+    }
+  }
+
   function retrieveBatch(dataSender, pageNumber) {
     params.page.number = pageNumber;
-    return new ResourcesGetter(model, options, params)
+    return getter()
       .perform()
       .then(function (results) {
         var records = results[1];
