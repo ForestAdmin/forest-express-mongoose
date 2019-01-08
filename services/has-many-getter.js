b3069fcb2db487d57c3a0eeb4e35c011446bcd0c
Oren Zomer <oren@awearsolutions.com>
2019-01-08 15:31:34 +0200

    ignore sort by field named parentsReferencingOrder

--- services/has-many-getter.js
+++ services/has-many-getter.js
@@ -86,8 +86,8 @@ function HasManyGetter(model, association, opts, params) {
       })
       .then(function(recordsAndRecordIds) {
         var records = recordsAndRecordIds[0];
-        if (params.sort) {
-          console.log(`params.sort is defined: ${JSON.stringify(params)} for records: ${records.map(r => r._id)}`);
+        if (params.sort && (params.sort !== 'parentsReferencingOrder') && (params.sort !== '-parentsReferencingOrder')) {
+          // console.log(`params.sort is defined: ${JSON.stringify(params)} for records: ${records.map(r => r._id)}`);
           var fieldSort = params.sort;
           var descending = false;
 
@@ -102,7 +102,7 @@ function HasManyGetter(model, association, opts, params) {
 
           return descending ? recordsSorted.reverse() : recordsSorted;
         }
-        console.log(`params.sort is not defined: ${JSON.stringify(params.sort)} for records: ${records.map(r => r._id)}`);
+        // console.log(`params.sort is not defined: ${JSON.stringify(params.sort)} for records: ${records.map(r => r._id)}`);
         var recordIds = recordsAndRecordIds[1];
         var recordIdStrings = recordIds.map(function(recordId) {
           // Convert values to strings, so ObjectIds could be easily searched and compared.
