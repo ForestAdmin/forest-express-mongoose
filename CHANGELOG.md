70a43d343d45a5ecbbdf5846cdf520da96cfa9b8
Vincent Molinié <vincent.molinie@epita.fr>
2019-07-26 13:30:19 +0200

    [*] ESLint - Fix `search-builder` according to eslint rules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## [Unreleased]
 ### Fixed
 - Technical - Fix pre-commit hook to avoid renamed file and add new lina at the end of the file.
+- ESLint - Fix `search-builder` according to eslint rules.
 
 ## RELEASE 3.2.4 - 2019-07-24
 ### Fixed
--- src/services/search-builder.js
+++ src/services/search-builder.js
@@ -1,19 +1,15 @@
-'use strict';
-var _ = require('lodash');
-var Interface = require('forest-express');
-var utils = require('../utils/schema');
+const _ = require('lodash');
+const Interface = require('forest-express');
+const utils = require('../utils/schema');
 
 function SearchBuilder(model, opts, params, searchFields) {
-  var schema = Interface.Schemas.schemas[utils.getModelName(model)];
-  var fieldsSearched = [];
+  const schema = Interface.Schemas.schemas[utils.getModelName(model)];
+  const fieldsSearched = [];
 
-  this.getFieldsSearched = function () {
-    return fieldsSearched;
-  };
+  this.getFieldsSearched = () => fieldsSearched;
 
-  this.getConditions = function () {
-
-    var orQuery = { $or: [] };
+  this.getConditions = () => {
+    const orQuery = { $or: [] };
 
     function pushCondition(condition, fieldName) {
       orQuery.$or.push(condition);
@@ -22,58 +18,58 @@ function SearchBuilder(model, opts, params, searchFields) {
 
     if (new RegExp('^[0-9a-fA-F]{24}$').test(params.search)) {
       return { _id: params.search };
-    } else {
+    }
+    _.each(model.schema.paths, (value, key) => {
+      if (searchFields && searchFields.indexOf(value.path) === -1) {
+        return;
+      }
 
-      _.each(model.schema.paths, function (value, key) {
-        if (searchFields && searchFields.indexOf(value.path) === -1) {
-          return null;
+      const condition = {};
+      const searchValue = params.search.replace(/[-[\]{}()*+!<=:?./\\^$|#\s,]/g, '\\$&');
+      const searchRegexp = new RegExp(`.*${searchValue}.*`, 'i');
+
+      if (value.instance === 'ObjectID') {
+        try {
+          condition[key] = opts.mongoose.Types.ObjectId(params.search);
+          pushCondition(condition, key);
+        } catch (error) {
+          console.error(error);
         }
-        var condition = {};
-
-        var searchValue = params.search.replace(/[-[\]{}()*+!<=:?./\\^$|#\s,]/g, '\\$&');
-        var searchRegexp = new RegExp('.*' + searchValue + '.*', 'i');
-
-        if (value.instance === 'ObjectID') {
-          try {
-            condition[key] = opts.mongoose.Types.ObjectId(params.search);
-            pushCondition(condition, key);
-          } catch(error) { return null; }
-        } else if (value.instance === 'String') {
+      } else if (value.instance === 'String') {
+        condition[key] = searchRegexp;
+        pushCondition(condition, key);
+      } else if (value.instance === 'Array') {
+        const field = _.find(schema.fields, { field: key });
+        if (field && _.isArray(field.type) && field.type[0] === 'String' &&
+          !field.reference) {
           condition[key] = searchRegexp;
           pushCondition(condition, key);
-        } else if (value.instance === 'Array') {
-          var field = _.find(schema.fields, { field: key });
-          if (field && _.isArray(field.type) && field.type[0] === 'String' &&
-            !field.reference) {
-            condition[key] = searchRegexp;
-            pushCondition(condition, key);
-          } else if (field && _.isArray(field.type) &&
-            !field.reference && parseInt(params.searchExtended)) {
-            var elemMatch = { $elemMatch: { $or: [], } };
+        } else if (field && _.isArray(field.type) &&
+          !field.reference && Number.parseInt(params.searchExtended, 10)) {
+          const elemMatch = { $elemMatch: { $or: [] } };
 
-            field.type[0].fields.forEach(function(subField) {
-              var query = {};
-              if (subField.type === 'String' &&
-                !value.schema.obj[subField.field].ref) {
-                query[subField.field] = searchRegexp;
-                elemMatch.$elemMatch.$or.push(query);
-              } else if (subField.type === 'Number' &&
-                parseInt(params.search)) {
-                query[subField.field] = parseInt(params.search);
-                elemMatch.$elemMatch.$or.push(query);
-              }
-            });
-            condition[key] = elemMatch;
-            pushCondition(condition, key);
-          }
+          field.type[0].fields.forEach((subField) => {
+            const query = {};
+            if (subField.type === 'String' &&
+              !value.schema.obj[subField.field].ref) {
+              query[subField.field] = searchRegexp;
+              elemMatch.$elemMatch.$or.push(query);
+            } else if (subField.type === 'Number' &&
+              Number.parseInt(params.search, 10)) {
+              query[subField.field] = Number.parseInt(params.search, 10);
+              elemMatch.$elemMatch.$or.push(query);
+            }
+          });
+          condition[key] = elemMatch;
+          pushCondition(condition, key);
         }
-      });
+      }
+    });
 
-      return orQuery.$or.length ? orQuery : {};
-    }
+    return orQuery.$or.length ? orQuery : {};
   };
 
-  this.getWhere = function (query) {
+  this.getWhere = (query) => {
     query.where(this.getConditions());
   };
 }
