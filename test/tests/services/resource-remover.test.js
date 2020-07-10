3d56b4390fa039b80ec42897af86b5e3f4174884
Raphaël Huchet <raphaelht@gmail.com>
2020-07-10 15:56:18 +0200

    test: add test on resource-remover

---
+++ test/tests/services/resource-remover.test.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+import loadFixture from 'mongoose-fixture-loader';
+import Interface from 'forest-express';
+import ResourceRemover from '../../../src/services/resource-remover';
+import mongooseConnect from '../../utils/mongoose-connect';
+
+describe('service > resource-remover', () => {
+  let IslandModel;
+
+  beforeAll(() => {
+    Interface.Schemas = {
+      schemas: {
+        Island: {
+          name: 'Island',
+          idField: '_id',
+          searchFields: ['name'],
+          fields: [
+            { field: '_id', type: 'String' },
+            { field: 'name', type: 'String' },
+          ],
+        },
+      },
+    };
+
+    return mongooseConnect()
+      .then(() => {
+        const IslandSchema = mongoose.Schema({
+          name: { type: String },
+        });
+
+        IslandModel = mongoose.model('Island', IslandSchema);
+        return IslandModel.remove({});
+      });
+  });
+
+  afterAll(() => mongoose.connection.close());
+
+  beforeEach(async () => {
+    await IslandModel.remove({});
+    await loadFixture(IslandModel, [
+      { name: 'Kauai', _id: '56cb91bdc3464f14678934ca' },
+      { name: 'Oahu', _id: '56cb91bdc3464f14678934cb' },
+    ]);
+  });
+
+
+  it('should not remove resource with missing recordId', async () => {
+    expect.assertions(1);
+    await new ResourceRemover(IslandModel, {}).perform();
+    const documentsCount = await IslandModel.countDocuments();
+    expect(documentsCount).toStrictEqual(2);
+  });
+
+  it('should remove one resource with existing ID', async () => {
+    expect.assertions(1);
+    await new ResourceRemover(IslandModel, { recordId: '56cb91bdc3464f14678934ca' }).perform();
+    const documentsCount = await IslandModel.countDocuments();
+    expect(documentsCount).toStrictEqual(1);
+  });
+
+  it('should not remove resource with not existing ID', async () => {
+    expect.assertions(1);
+    await new ResourceRemover(IslandModel, { recordId: '56cb91bdc3464f14678934cd' }).perform();
+    const documentsCount = await IslandModel.countDocuments();
+    expect(documentsCount).toStrictEqual(2);
+  });
+});
