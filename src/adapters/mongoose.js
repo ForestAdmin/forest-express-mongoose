c1fcd6e39a7e71e41ab1498245b79e940121cbcd
Jeff LADIRAY <ladirayjeff@gmail.com>
2020-07-28 08:58:01 +0200

    fix(adapter): avoid serializing enum function on Number type (#384)

--- src/adapters/mongoose.js
+++ src/adapters/mongoose.js
@@ -73,11 +73,6 @@ module.exports = (model, opts) => {
         field.enums = fieldInfo.enumValues;
       }
 
-      // NOTICE: Detect enum values for Enums in subdocument arrays.
-      if (fieldInfo.enum && fieldInfo.enum.length) {
-        field.enums = fieldInfo.enum;
-      }
-
       type.fields.push(field);
     });
 
