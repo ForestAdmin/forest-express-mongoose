3f6b55a32064d370cfc648393c22254ac48d2c4c
Jeff LADIRAY <ladirayjeff@gmail.com>
2020-10-27 16:25:48 +0100

    fix: switched moment to moment-tz for line-stat-getter (#428)

--- src/services/line-stat-getter.js
+++ src/services/line-stat-getter.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import _ from 'lodash';
 import P from 'bluebird';
-import moment from 'moment';
-// NOTICE: moment-timezone extends moment itself,
-//         Importing it will automatically add functions to moment.
-import 'moment-timezone';
+import moment from 'moment-timezone';
 import Interface from 'forest-express';
 import QueryBuilder from './query-builder';
 import utils from '../utils/schema';
