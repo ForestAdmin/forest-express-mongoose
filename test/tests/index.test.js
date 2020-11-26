bf87bbff4e2b61d1ea7305a0a68f809d55bef98e
Jeff LADIRAY <ladirayjeff@gmail.com>
2020-11-26 12:04:22 +0100

    test(interface): add exported interface test (#447)

---
+++ test/tests/index.test.js
@@ -0,0 +1,69 @@
+const forestExpressMongoose = require('../../src/index');
+
+describe('index', () => {
+  describe('exported Interface', () => {
+    it('should export a collection function', () => {
+      expect.assertions(2);
+
+      expect(forestExpressMongoose.collection).toBeDefined();
+      expect(forestExpressMongoose.collection).toBeInstanceOf(Function);
+    });
+
+    it('should export an ensureAuthenticated middleware', () => {
+      expect.assertions(2);
+
+      expect(forestExpressMongoose.ensureAuthenticated).toBeDefined();
+      expect(forestExpressMongoose.ensureAuthenticated).toBeInstanceOf(Function);
+    });
+
+    it('should export a list of serializers and deserializers', () => {
+      expect.assertions(4);
+
+      expect(forestExpressMongoose.StatSerializer).toBeDefined();
+      expect(forestExpressMongoose.StatSerializer).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.ResourceSerializer).toBeDefined();
+      expect(forestExpressMongoose.ResourceSerializer).toBeInstanceOf(Function);
+    });
+
+    it('should export a list of records functions', () => {
+      expect.assertions(20);
+
+      expect(forestExpressMongoose.PermissionMiddlewareCreator).toBeDefined();
+      expect(forestExpressMongoose.PermissionMiddlewareCreator).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordsCounter).toBeDefined();
+      expect(forestExpressMongoose.RecordsCounter).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordsExporter).toBeDefined();
+      expect(forestExpressMongoose.RecordsExporter).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordsGetter).toBeDefined();
+      expect(forestExpressMongoose.RecordsGetter).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordGetter).toBeDefined();
+      expect(forestExpressMongoose.RecordGetter).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordUpdater).toBeDefined();
+      expect(forestExpressMongoose.RecordUpdater).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordCreator).toBeDefined();
+      expect(forestExpressMongoose.RecordCreator).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordRemover).toBeDefined();
+      expect(forestExpressMongoose.RecordRemover).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordsRemover).toBeDefined();
+      expect(forestExpressMongoose.RecordsRemover).toBeInstanceOf(Function);
+
+      expect(forestExpressMongoose.RecordSerializer).toBeDefined();
+      expect(forestExpressMongoose.RecordSerializer).toBeInstanceOf(Function);
+    });
+
+    it('should export the PUBLIC_ROUTES', () => {
+      expect.assertions(2);
+      expect(forestExpressMongoose.PUBLIC_ROUTES).toBeDefined();
+      expect(forestExpressMongoose.PUBLIC_ROUTES).toBeInstanceOf(Array);
+    });
+  });
+});
