f8656f6740a78caafc8b6ac1c93e75942873581f
SteveBunlon <SteveBunlon@users.noreply.github.com>
2021-11-30 14:12:51 +0100

    fix: add searchFields type in the collection options (#733)

--- types/index.d.ts
+++ types/index.d.ts
@@ -325,6 +325,7 @@ export interface CollectionOptions {
   fields?: SmartFieldOptions[];
   actions?: SmartActionOptions[];
   segments?: SmartSegmentOptions[];
+  searchFields?: string[];
   fieldsToFlatten?: ({ field: string, level?: number } | string)[]
 }
 
