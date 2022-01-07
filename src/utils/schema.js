2bb7ee828bf2591fe4b7360c68a78210e2405773
SteveBunlon <SteveBunlon@users.noreply.github.com>
2022-01-07 10:55:37 +0100

    fix: filters on nested fields now works correctly (#763)

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -13,3 +13,29 @@ exports.getModelName = (model) => model.modelName;
 
 // TODO: Remove nameOld attribute once the lianas versions older than 2.0.0 are minority
 exports.getModelNameOld = (model) => model.collection.name.replace(' ', '');
+
+const getNestedFieldType = (mongooseSchema, nestedFieldPath) => {
+  if (!mongooseSchema || !nestedFieldPath) return undefined;
+
+  const [currentFieldName, ...deepNestedFieldPath] = nestedFieldPath.split('.');
+
+  let nestedFieldDeclaration;
+
+  if (mongooseSchema.tree) {
+    nestedFieldDeclaration = mongooseSchema.tree[currentFieldName];
+  } else if (mongooseSchema.type) {
+    nestedFieldDeclaration = mongooseSchema.type[currentFieldName];
+  } else {
+    nestedFieldDeclaration = mongooseSchema[currentFieldName];
+  }
+
+  if (!nestedFieldDeclaration) return undefined;
+
+  if (!deepNestedFieldPath.length) {
+    return nestedFieldDeclaration.type || nestedFieldDeclaration;
+  }
+
+  return getNestedFieldType(nestedFieldDeclaration, deepNestedFieldPath?.join('.'));
+};
+
+exports.getNestedFieldType = getNestedFieldType;
---
+++ test/tests/utils/schema.test.js
@@ -0,0 +1,130 @@
+import mongoose from 'mongoose';
+import mongooseConnect from '../../utils/mongoose-connect';
+import { getNestedFieldType } from '../../../src/utils/schema';
+
+describe('schema', () => {
+  let nestedModelSchema;
+
+  beforeAll(async () => {
+    await mongooseConnect();
+
+    nestedModelSchema = mongoose.model('cars', new mongoose.Schema({
+      engineSubSchemaAndType: new mongoose.Schema({
+        horsePower: { type: String },
+        _id: { type: mongoose.Schema.Types.ObjectId },
+      }),
+      engineSubSchema: new mongoose.Schema({
+        horsePower: String,
+        _id: mongoose.Schema.Types.ObjectId,
+      }),
+      engineNestedDocumentAndType: {
+        type: {
+          horsePower: { type: String },
+          _id: { type: mongoose.Schema.Types.ObjectId },
+        },
+      },
+      engineNestedDocument: {
+        type: {
+          horsePower: String,
+          _id: mongoose.Schema.Types.ObjectId,
+        },
+      },
+      engineNestedPathAndType: {
+        horsePower: { type: String },
+        _id: { type: mongoose.Schema.Types.ObjectId },
+      },
+      engineNestedPath: {
+        horsePower: String,
+        _id: mongoose.Schema.Types.ObjectId,
+      },
+    })).schema;
+  });
+
+  afterAll(() => mongoose.connection.close());
+
+  describe('when the nested field is from a subSchema', () => {
+    describe('when the nested field is defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineSubSchemaAndType._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineSubSchemaAndType.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+    describe('when the nested field is not defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineSubSchema._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineSubSchema.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+  });
+
+  describe('when the nested field is from a nested document', () => {
+    describe('when the nested field is defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedDocumentAndType._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedDocumentAndType.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+    describe('when the nested field is not defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedDocument._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedDocument.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+  });
+
+
+  describe('when the nested field is from a nested path', () => {
+    describe('when the nested field is defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedPathAndType._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedPathAndType.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+    describe('when the nested field is not defined using a type', () => {
+      it('should correctly detect the type', () => {
+        expect.assertions(2);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedPath._id'),
+        ).toStrictEqual(mongoose.Schema.Types.ObjectId);
+        expect(
+          getNestedFieldType(nestedModelSchema, 'engineNestedPath.horsePower'),
+        ).toStrictEqual(String);
+      });
+    });
+  });
+
+  describe('when the nested field is not found', () => {
+    it('should return undefined', () => {
+      expect.assertions(1);
+
+      expect(
+        getNestedFieldType(nestedModelSchema, 'engineNestedPath.notExisting'),
+      ).toBeUndefined();
+    });
+  });
+});
